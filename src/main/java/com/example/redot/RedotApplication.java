928f52e240f71b98b77c6f4d9e5672d55f4cf807
DongHunKim <dh.kim@bfactory.inc>
2021-08-09 14:45:52 +0900

    first commit

---
+++ src/main/java/com/example/redot/RedotApplication.java
@@ -0,0 +1,13 @@
+package com.example.redot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redot/RedotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
